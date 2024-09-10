59c52fb8a72f13c1e5f45c7014a173ea8d5e3aef
Mezanic <ezanic.mael@gmail.com>
2024-09-10 12:27:16 +0200

    Pycode style

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
 
     tuple_a = (len(sentence), sentence[0])
 
-    if len(sentence) <= 0:
+    if len(sentence) < 0:
         last_char = "None"
 
     return (tuple_a)
