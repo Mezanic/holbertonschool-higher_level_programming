add5e713f54583ed71e86ddd91ef7b47b772e2b3
Mezanic <ezanic.mael@gmail.com>
2024-09-10 13:36:10 +0200

    change return value

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-
-    tuple_a = (len(sentence), sentence[0])
-
+   
     if len(sentence) <= 0:
         last_char = "None"
 
-    return (tuple_a)
+    return (len(sentence, sentence[0]))
