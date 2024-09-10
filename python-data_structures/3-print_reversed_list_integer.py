bb2e625b5735dd1441150bda892937a978464515
Mezanic <ezanic.mael@gmail.com>
2024-09-10 02:48:45 +0200

    added :d for format decimal number

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -4,4 +4,9 @@ def print_reversed_list_integer(my_list=[]):
 
     my_list = reversed(my_list)
     for i in my_list:
-        print("{}" .format(i))
+        print("{:d}" .format(i))
+
+
+
+my_list = [1, 2, 3, 4, 5]
+print_reversed_list_integer(my_list)
