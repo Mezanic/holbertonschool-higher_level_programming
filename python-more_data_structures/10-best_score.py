f61b42aa1639369886473a7d12c9da683cbf56d0
Mezanic <ezanic.mael@gmail.com>
2024-09-12 00:59:11 +0200

    change the way to check if the dictonnary is empty or not

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
-        return None
 
-    return max(a_dictionary, key=a_dictionary.get)
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get)
+
+    return None
