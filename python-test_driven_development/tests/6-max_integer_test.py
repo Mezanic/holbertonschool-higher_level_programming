6697dc670724607308f63f42034a8913f176fa82
Mezanic <ezanic.mael@gmail.com>
2024-09-13 23:58:08 +0200

    Refactored code thx TM

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
-"""
+"""Unittest for max_integer([..])"""
 
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+class TestMaxInteger(unittest.TestCase):
+    """Test cases for the max_integer function"""
+
+    def test_max_at_end(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_max_at_middle(self):
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+
+    def test_max_at_beginning(self):
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+
+    def test_one_element(self):
+        self.assertEqual(max_integer([90]), 90)
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-5, -9, -2, -42]), -2)
+
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([-11, 15, 22, -3]), 22)
+
+    def test_all_same(self):
+        self.assertEqual(max_integer([6, 6, 6, 6]), 6)
+
+if __name__ == "__main__":
+    unittest.main()
