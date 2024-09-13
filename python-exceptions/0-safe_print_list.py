a153cb4226d976b5981053a98d0c3085dc30f1ee
Mezanic <ezanic.mael@gmail.com>
2024-09-13 12:00:49 +0200

    Refactored code, place the for before the try, changed the except and brea now, add count for count how many element are printed

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
+    for num in range(x):
 
-    try:
-        for num in range(x):
+        try:
             print(my_list[num], end="")
-        print()
-        return (num + 1)
+            count += 1
 
-    except IndexError:
-        print()
-        return num
+        except IndexError:
+            break
+
+    print()
+    return count
