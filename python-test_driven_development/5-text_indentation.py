8f39a9bab51f428d03bff89c80fb6de208226137
Mezanic <ezanic.mael@gmail.com>
2024-09-13 23:52:27 +0200

    Added file and unit test for task 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Module for text_indentation method """
+
+
+def text_indentation(text):
+    """ Method to print a text with 2 new lines after each of
+    these characters: ., ? and :
+
+    Args:
+        text: text to be printed
+
+    Raises:
+        TypeError: if text is not a string
+
+    Returns:
+        None
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    index = 0
+    while index < len(text) and text[index] == ' ':
+        index += 1
+
+    while index < len(text):
+        print(text[index], end="")
+        if text[index] == "\n" or text[index] in ".?:":
+            if text[index] in ".?:":
+                print("\n")
+            index += 1
+            while index < len(text) and text[index] == ' ':
+                index += 1
+            continue
+        index += 1
