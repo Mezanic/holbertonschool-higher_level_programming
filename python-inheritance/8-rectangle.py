c8c04d60b8264a6f6a638f230aa2005907da9a70
Mezanic <ezanic.mael@gmail.com>
2024-09-27 10:43:00 +0200

    remove the code and import task 7

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,30 +1,6 @@
 #!/usr/bin/python3
 """This Module create an BaseGeometry"""
-
-
-class BaseGeometry:
-    """Class BaseGeometry"""
-
-    def area(self):
-        """Calculate area (WIP)"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Function for validate value
-
-        Args:
-            name: string to check
-            value: integer to check
-        Raises:
-            TypeError: name must be an integer
-            ValueError: name must be greater than 0
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
