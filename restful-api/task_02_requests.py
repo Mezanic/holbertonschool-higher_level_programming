bddffbaa357e4622fc40df4139d8f5b7ca9f1233
Mezanic <ezanic.mael@gmail.com>
2024-10-11 10:52:08 +0200

    changed thing for pleasure of the Checker

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """Script to fetch posts from JSONPlaceholde"""
 
-
 import csv
 import requests
+
+
+response = requests.get("https://jsonplaceholder.typicode.com/posts")
 
 
 def fetch_and_print_posts():
     """ Function to print title of post in jsonplaceholder """
 
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
-
     print(f"Status Code: {response.status_code}")
 
-    if response.status_code == 200:
+    if response:
         posts = response.json()
-
         for post in posts:
             print(post["title"])
 
@@ -26,15 +25,16 @@ def fetch_and_save_posts():
     and write in a csv files
     """
 
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
-
-    if response.status_code == 200:
+    if response:
         posts = response.json()
 
-        data = [{"id": post["id"], "title": post["title"], "body": post["body"]
-                 } for post in posts]
+        data = [{
+            "id": post["id"],
+            "title": post["title"],
+            "body": post["body"]
+        } for post in posts]
 
-    with open("post.csv", mode="w", newline="") as csvfile:
+    with open("posts.csv", mode="w", newline="") as csvfile:
         fieldnames = ["id", "title", "body"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
