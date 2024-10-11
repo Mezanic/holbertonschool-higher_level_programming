8fc94ec1142f8307ecfdf65c806cb19fc5a19fb7
Mezanic <ezanic.mael@gmail.com>
2024-10-11 10:10:41 +0200

    Follow Saint Thomas guidance

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """Script to fetch posts from JSONPlaceholde"""
 
+
 import csv
 import requests
-
-
-response = requests.get("https://jsonplaceholder.typicode.com/posts")
-
 
 def fetch_and_print_posts():
     """ Function to print title of post in jsonplaceholder """
 
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
     print(f"Status Code: {response.status_code}")
 
     if response.status_code == 200:
@@ -25,6 +23,8 @@ def fetch_and_save_posts():
     and write in a csv files
     """
 
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    
     if response.status_code == 200:
         posts = response.json()
 
