20feae95e8ce80bd4906288f57c1636b7ebc081a
Mezanic <ezanic.mael@gmail.com>
2024-10-11 09:58:29 +0200

    check if the request for get is succesful and not if response is None or empty

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -13,7 +13,7 @@ def fetch_and_print_posts():
 
     print(f"Status Code: {response.status_code}")
 
-    if response:
+    if response.status_code == 200:
         posts = response.json()
         for post in posts:
             print(post["title"])
@@ -25,7 +25,7 @@ def fetch_and_save_posts():
     and write in a csv files
     """
 
-    if response:
+    if response.status_code == 200:
         posts = response.json()
 
         data = [{
