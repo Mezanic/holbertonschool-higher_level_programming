e989cb17e768e9b0371f73a2700b0a4e8f35d4b1
Mezanic <ezanic.mael@gmail.com>
2024-10-30 13:48:57 +0100

    Remove simple quote

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- Create my first tables in SQL 
-CREATE TABLE IF NOT EXISTS 'first_table'('id' INT NOT NULL, 'name' VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS first_table(id INT NOT NULL, name VARCHAR(256) NOT NULL);
