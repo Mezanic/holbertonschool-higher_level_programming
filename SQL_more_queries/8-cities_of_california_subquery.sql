49d45d917e77820b8caf62e6d6b6fb0663c2641b
Mezanic <ezanic.mael@gmail.com>
2024-10-31 15:13:52 +0100

    First attempt for task 8

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- Lists all the cities of California that can be found in the database 
+SELECT cities.id, cities.name FROM hbtn_0d_usa;
+WHERE cities.state_id (SELECT id FROM states
+WHERE name = "California" )
+ORDER BY cities.id ASC;
