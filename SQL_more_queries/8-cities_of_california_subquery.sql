7fd00b11773a1bfc7a485d86678d023e134f2fa0
Mezanic <ezanic.mael@gmail.com>
2024-10-31 15:17:33 +0100

    Remove after from hbtn_0d_usa

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- Lists all the cities of California that can be found in the database 
-SELECT cities.id, cities.name FROM hbtn_0d_usa;
+SELECT cities.id, cities.name FROM hbtn_0d_usa
 WHERE cities.state_id (SELECT id FROM states
 WHERE name = "California" )
 ORDER BY cities.id ASC;
