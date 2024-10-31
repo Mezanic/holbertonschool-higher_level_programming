4eab548a75afc7192cf30698b479dcd71a79172d
Mezanic <ezanic.mael@gmail.com>
2024-10-31 14:11:06 +0100

    use good name for variable

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Create table id not null and unique 
-CREATE TABLE IF NOT EXISTS id_not_null (id INT UNIQUE DEFAULT 1, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256) NOT NULL);
