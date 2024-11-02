05422b4e783c4efc8c114f6580d15d770a8f54ab
Mezanic <ezanic.mael@gmail.com>
2024-11-02 20:53:03 +0100

    Update documentation

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
 
-"""lists all states that have a specific name"""
+"""
+A script that lists all states from the database hbtn_0e_0_usa where
+the name matches the user-provided argument, safe from SQL injection.
+
+The script takes four arguments:
+1. MySQL username
+2. MySQL password
+3. Database name
+4. State name to search for
+
+Usage:
+    ./3-my_safe_filter_states.py
+    <mysql_username> <mysql_password> <database_name> <state_name>
+
+The script connects to a MySQL server running on localhost at port 3306
+and retrieves states with a name matching the user
+input in ascending order by id.
+"""
 
 import sys
 import MySQLdb
