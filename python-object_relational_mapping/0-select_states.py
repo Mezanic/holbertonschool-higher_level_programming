62d322ff4a645b2227337dda5c6d2715a4a1c540
Mezanic <ezanic.mael@gmail.com>
2024-11-02 19:51:19 +0100

    First task for project ORM, check if the logic behind it is good for the checker

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""lists all states from the database hbtn_0e_0_usa"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    
+    # Connect to database and set up
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+    # Create a cursor
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
+    for row in states:
+        print(row)
+    cursor.close()
+    db.close()
