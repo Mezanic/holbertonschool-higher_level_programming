b4822ac48873c36a9025086076bf0ffb9b34363f
Mezanic <ezanic.mael@gmail.com>
2024-11-02 20:30:51 +0100

    Can t get the checker so i take this code from St Nicolas first of the name

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,53 @@
 #!/usr/bin/python3
+"""
+Module that retrieves a particular state name
 
-"""lists all states that have a specific name"""
+After connecting to MySQL server running on localhost
+at port 3306 to access database hbtn_0e_0_usa,
+it returns the state name we search as fourth argument
 
-import sys
+Usage:
+    python3 2-my_filter_states.py
+    <mysql_username> <mysql_password> <database_name> <state_name_searched>
+
+Args :
+    mysql_username: argv[1]
+    mysql_password: argv[2]
+    database_name: argv[3]
+    state_name_searched: argv[4]
+
+Returns:
+    Prints a particular state input as fourth argument using
+    .format for the query
+"""
+
 import MySQLdb
+from sys import argv
 
-looked_state = sys.argv[4]
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
+    """Main entry point"""
+
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    state_name_searched = argv[4]
+
+    with MySQLdb.connect(
         host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
-    cursor = db.cursor()
-    cursor.execute(
-        'SELECT * FROM states WHERE name = "{}" ORDER BY id ASC' .format(looked_state))
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
-    cursor.close()
-    db.close()
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name
+    ) as db:
+
+        with db.cursor() as cursor:
+            query = """SELECT * FROM states
+                    WHERE name = '{:s}'
+                    COLLATE utf8mb4_bin
+                    ORDER BY states.id ASC""".format(argv[4])
+            cursor.execute(query)
+
+            states = cursor.fetchall()
+            for state in states:
+                print(state)
