c5841fe64b876816110f20076f6bf8fb235b50b7
Mezanic <ezanic.mael@gmail.com>
2024-11-02 21:38:03 +0100

    Changed the query

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -24,34 +24,33 @@ Returns:
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    """Main entry point"""
-    
-    username = sys.argv[1]
-    password = sys.argv[2]
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
     database_name = sys.argv[3]
     state_name = sys.argv[4]
 
     db = MySQLdb.connect(
         host="localhost",
-        user=username,
-        passwd=password,
-        db=database_name,
-        port=3306
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
     )
 
     cursor = db.cursor()
 
     query = """
-            SELECT cities.name FROM cities
-            JOIN states ON cities.state_id = states.id
-            WHERE states.name = %s
-            ORDER BY cities.id ASC
-        """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """
     cursor.execute(query, (state_name,))
-    cities = cursor.fetchall()
 
+    cities = cursor.fetchall()
     print(", ".join(city[0] for city in cities))
 
     cursor.close()
