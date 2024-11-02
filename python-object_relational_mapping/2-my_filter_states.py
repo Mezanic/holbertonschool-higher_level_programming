6d53a2c577ccddd46732afd363be93f870f75d2f
Mezanic <ezanic.mael@gmail.com>
2024-11-02 20:17:16 +0100

    Added task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""lists all states that have a specific name"""
+
+import sys
+import MySQLdb
+
+looked_state = sys.argv[4]
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+    cursor = db.cursor()
+    cursor.execute(f"SELECT * FROM states WHERE name = {looked_state} ORDER BY id ASC")
+    states = cursor.fetchall()
+    for state in states:
+        if state[1] == sys.argv[4]:
+            print(state)
+    cursor.close()
+    db.close()
