fc73bb2026cdd5edf077328604354ba86dd67f0f
Mezanic <ezanic.mael@gmail.com>
2024-11-02 20:22:14 +0100

    remove f string for string format

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     )
     cursor = db.cursor()
     cursor.execute(
-        f"SELECT * FROM states WHERE name = '{looked_state}' ORDER BY id ASC")
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC" .format(looked_state))
     states = cursor.fetchall()
     for state in states:
         if state[1] == sys.argv[4]:
